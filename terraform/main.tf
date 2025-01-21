62acbb3fc96a6a4456184d261c9022e487cdb486
SalllesAndr <salllesandrr@gmail.com>
2025-01-20 22:24:40 -0300

    ADD new script main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,7 +66,13 @@ resource "aws_security_group" "main" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+ ingress {
+    description = "Allow Zabbix Web Port"
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   ingress {
     description = "Allow Zabbix Agent Port"
     from_port   = 10051
